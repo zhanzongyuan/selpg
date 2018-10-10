281b04e9aa9df9c912894f4f281e5d9454f7f057
zhanzongyuan <zhanzy5@mail2.sysu.edu.cn>
2018-10-10 15:17:27 +0800

    Update goroutine channel type to error type.

--- selpg.go
+++ selpg.go
@@ -118,42 +118,41 @@ func processStream(in io.Reader, out io.Writer) error {
 }
 
 // printer goroutine
-func runPrinter(reader io.Reader, quit chan int) {
-	defer func() {
-		quit <- 0
-	}()
+func runPrinter(reader io.Reader, quit chan error) {
 	cmd := exec.Command("lp", "-d", *destination)
 	cmd.Stdin = reader
 
 	// create command standard output and input output reader
 	stdoutReader, err := cmd.StdoutPipe()
 	if err != nil {
-		exitCode = 2
-		log.Fatal(err)
+		quit <- err
+		return
 	}
+
 	stderrReader, err := cmd.StderrPipe()
 	if err != nil {
-		exitCode = 2
-		log.Fatal(err)
+		quit <- err
+		return
 	}
 
 	// start command and wait
 	if err := cmd.Start(); err != nil {
-		exitCode = 2
-		log.Fatal(err)
+		quit <- err
+		return
 	}
 	if _, err := io.Copy(os.Stdout, stdoutReader); err != nil {
-		exitCode = 2
-		log.Fatal(err)
+		quit <- err
+		return
 	}
 	if _, err := io.Copy(os.Stderr, stderrReader); err != nil {
-		exitCode = 2
-		log.Fatal(err)
+		quit <- err
+		return
 	}
 	if err := cmd.Wait(); err != nil {
-		exitCode = 2
-		log.Fatal(err)
+		quit <- err
+		return
 	}
+	quit <- nil
 }
 
 func reportErr(err error) {
@@ -197,7 +196,7 @@ func selpgMain() {
 
 	// switch output writer to (os.Stdout | lp -d)
 	var out io.Writer
-	quit := make(chan int)
+	quit := make(chan error)
 	if *destination == "" {
 		out = os.Stdout
 	} else {
@@ -207,7 +206,10 @@ func selpgMain() {
 		go runPrinter(reader, quit)
 		defer func() {
 			writer.Close()
-			<-quit
+			if err := <-quit; err != nil {
+				exitCode = 2
+				log.Fatal(err)
+			}
 		}()
 	}
 
